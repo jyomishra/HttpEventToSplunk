73f5d15970f82e167991d857971ace6fd5a92e41
Jyotirmay MMishra <jyotirmay.cse@gmail.com>
2021-03-02 12:17:13 +0000

    Test Commit

---
+++ New-Http-Trigger-Splunk/AnotherFuntion.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace New_Http_Trigger_Splunk
+{
+    public static class AnotherFuntion
+    {
+        [FunctionName("AnotherFuntion")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Ok");
+        }
+    }
+}
